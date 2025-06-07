1054598e994ae7b8feca7310ca93ce15495c726a
timbass <timbass1@att.net>
2025-06-06 20:33:03 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,26 @@
-const CACHE_NAME = 'gt-suite-v1';
+const CACHE_NAME = 'gt-suite-v2'; // Change version number
 const urlsToCache = [
   '/',
   '/index.html',
   '/manifest.json'
 ];
 
-self.addEventListener('install', event => {
+// Add update notification
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+// Skip waiting to activate new version
+self.addEventListener('message', event => {
+  if (event.data === 'skipWaiting') {
+    self.skipWaiting();
+  }
 });
